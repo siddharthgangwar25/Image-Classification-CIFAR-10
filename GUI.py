c3abeb8c6c4d3e618b2f9b96c8e76c70ddb7b145
Siddharth Gangwar <gangwarsiddharth25@gmail.com>
2021-05-05 14:45:02 +0530

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,82 @@
+#Importing libraries
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk, Image
+import numpy
+
+#Load the trained model to classify the images
+from keras.models import load_model
+model = load_model('model1_cifar_10epoch.h5')
+
+#Dictionary to label the CIFAR-10 dataset classes
+classes = {
+    0:'Aeroplane',
+    1:'Automobile',
+    2:'Bird',
+    3:'Cat',
+    4:'Deer',
+    5:'Dog',
+    6:'Frog',
+    7:'Horse',
+    8:'Ship',
+    9:'Truck'
+}
+
+#Initialising GUI
+top=tk.Tk()
+top.geometry('800x600')
+top.title('Image Classification CIFAR10')
+top.configure(background='#CDCDCD')
+label=Label(top,background='#CDCDCD', font=('arial',15,'bold'))
+sign_image = Label(top)
+
+def classify(file_path):
+    global label_packed
+    from keras.preprocessing.image import load_img
+    from keras.preprocessing.image import img_to_array
+    import numpy as np
+    img = load_img(file_path, target_size=(32, 32))
+    # convert to array
+    img = img_to_array(img)
+    # reshape into a single sample with 3 channels
+    img = img.reshape(1, 32, 32, 3)
+    # prepare pixel data
+    img = img.astype('float32')
+    img = img / 255.0
+
+    pred = np.argmax(model.predict(img), axis=-1)
+    print(pred, classes[pred[0]])
+    label.configure(foreground='#011638', text=classes[pred[0]])
+
+def show_classify_button(file_path):
+    classify_b=Button(top,text="Classify Image",
+   command=lambda: classify(file_path),padx=10,pady=5)
+    classify_b.configure(background='#364156', foreground='white',
+font=('arial',10,'bold'))
+    classify_b.place(relx=0.79,rely=0.46)
+
+def upload_image():
+    try:
+        file_path=filedialog.askopenfilename()
+        uploaded=Image.open(file_path)
+        uploaded.thumbnail(((top.winfo_width()/2.25),(top.winfo_height()/2.25)))
+        im=ImageTk.PhotoImage(uploaded)
+        sign_image.configure(image=im)
+        sign_image.image=im
+        label.configure(text='')
+        show_classify_button(file_path)
+    except:
+        pass
+
+upload=Button(top,text="Upload an image",command=upload_image,
+  padx=10,pady=5)
+upload.configure(background='#364156', foreground='white',
+    font=('arial',10,'bold'))
+upload.pack(side=BOTTOM,pady=50)
+sign_image.pack(side=BOTTOM,expand=True)
+label.pack(side=BOTTOM,expand=True)
+heading = Label(top, text="Image Classification CIFAR-10",pady=20, font=('arial',20,'bold'))
+heading.configure(background='#CDCDCD',foreground='#364156')
+heading.pack()
+top.mainloop()
